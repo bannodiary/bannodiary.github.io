bb4e161959fcef036818707d2e4298e63fddb035
bannodiary <prakatkit@gmail.com>
2024-10-25 06:51:00 +0700

    update_minigame_test

--- test0/offline.js
+++ test0/offline.js
@@ -1,14 +1,17 @@
 ﻿{
-	"version": 1729767066,
+	"version": 1729813773,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-3.4.1.min.js",
 		"offlineClient.js",
 		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
 		"images/sprite2-sheet0.png",
 		"images/sprite3-sheet0.png",
 		"images/sprite4-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite5-sheet1.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
